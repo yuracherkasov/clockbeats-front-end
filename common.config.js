be5c7724279b84347a6887ef4eb0177bac112f33
Serhii Yaitsky <sergey@code-care.pro>
2017-10-09 21:19:00 +0300

    Fcku

--- common.config.js
+++ common.config.js
@@ -1,3 +1,5 @@
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
-export const ORIGIN = PRODUCTION ? 'https://api-clockbeats-staging.herokuapp.com' : 'http://localhost:8080';
+module.exports = {
+	ORIGIN: PRODUCTION ? 'https://api-clockbeats-staging.herokuapp.com' : 'http://localhost:8080',
+};
