1c5e85094b29db31fd03fe98eddef219c358d505
Serhii Yaitsky <sergey@code-care.pro>
2017-10-09 21:16:57 +0300

    Disable uglifyjs

--- webpack.config.js
+++ webpack.config.js
@@ -236,20 +236,20 @@ if (PRODUCTION) {
 
 	config.plugins.push(
 		new WebpackMd5Hash(),
-		new UglifyJsPlugin({
-			compress: {
-				warnings: false,
-				// Disabled because of an issue with Uglify breaking seemingly valid code:
-				// https://github.com/facebookincubator/create-react-app/issues/2376
-				// Pending further investigation:
-				// https://github.com/mishoo/UglifyJS2/issues/2011
-				comparisons: false,
-			},
-			output: {
-				comments: false,
-			},
-			sourceMap: true,
-		}),
+		// new UglifyJsPlugin({
+		// 	compress: {
+		// 		warnings: false,
+		// 		// Disabled because of an issue with Uglify breaking seemingly valid code:
+		// 		// https://github.com/facebookincubator/create-react-app/issues/2376
+		// 		// Pending further investigation:
+		// 		// https://github.com/mishoo/UglifyJS2/issues/2011
+		// 		comparisons: false,
+		// 	},
+		// 	output: {
+		// 		comments: false,
+		// 	},
+		// 	sourceMap: true,
+		// }),
 
 
 		// Generate a service worker script that will precache, and keep up to date,
